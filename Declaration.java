275edc7cdb510eca8cc22d019b18d5ce3487ef23
Amandine <amandinefouillet@gmail.com>
2014-03-18 09:41:44 +0100

    Commentaires2

--- Declaration.java
+++ Declaration.java
@@ -1,27 +1,33 @@
 public class Declaration{
 	
+	/* ça se passe de commentaires ! Mais on va faire un effort*/
+	
 	private static String identAStocker;
 	private static String typeAStocker;
         private static int entierAStocker;
         private static int offset;
         
+       /*Pour chaque constante ou variable, on stocke son type, son nom et sa valeur ( offset pour les variables)*/
+        
         
         public void stockerIdent(){
         	this.identAStocker = YakaTokenManager.identLu;
-        }
+        }//nom
         
         public void stockerEntier(int n){
         	this.entierAStocker = n;
-        }
+        }//valeur pour les constantes
         
         public void stockerType(String type){
         	this.typeAStocker = type;
-        }
+        }//type
         
         public void calculerOffset(){
         	this.offset = this.offset-2;
-        }
+        }//incrémentation de l'offset pour les variables
         
+        /* On regroupe finalement toutes les informations dans un tableau qu'on pourra afficher */
+      
         public void remplirTableauVar(){
         	IdVar idVar =  new IdVar(this.typeAStocker,this.offset);
   	   	Yaka.tabIdent.rangeIdent(this.identAStocker,idVar);
